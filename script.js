8cfb3bb02f212fd12302fd988c62e1e049f5cc3a
Retosila <darksky1010@naver.com>
2024-07-09 00:15:28 +0900

    Remove blur from video emoticons

--- script.js
+++ script.js
@@ -14,5 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
     img.classList.remove('blur');
   });
   console.log(`Unblurred Emoticons: ${imgs.length}`)
+
+  // Remove blur from video emoticons
+  const videos = document.querySelectorAll('video.blur');
+  videos.forEach(video => {
+    video.classList.remove('blur');
+  });
+  console.log(`Unblurred Video Emoticons: ${videos.length}`)
 });
   
