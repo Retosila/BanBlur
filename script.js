c3d64289f2b1ddadf6f0c4bce355f951467f7164
reto <98384149+Retosila@users.noreply.github.com>
2024-08-25 03:33:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
+console.info("Ban Blur가 활성화되었습니다!");
+
 let debounceTimer;
-
-function removeBlur() {
-  const blurredElements = document.querySelectorAll(".blur");
-  blurredElements.forEach((element) => {
-    element.classList.remove("blur");
-  });
-  console.info(
-    `${blurredElements.length}개의 블러된 컨텐츠를 정상화하였습니다!`
-  );
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  console.info("BanBlur가 활성화되었습니다!");
-
-  removeBlur();
-
-  const observer = new MutationObserver(() => {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(removeBlur, 3_000);
-  });
-
-  observer.observe(document.body, { childList: true, subtree: true });
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    const blurredElements = document.querySelectorAll(".blur");
+    blurredElements.forEach((element) => {
+      element.classList.remove("blur");
+    });
+    console.info(
+      `${blurredElements.length}개의 블러된 컨텐츠를 정상화하였습니다!`
+    );
+  }, 1_000);
 });
